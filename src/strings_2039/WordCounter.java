0219c8a3a4b472881a7fd1a30cf0c900194d4a71
Votun <cabaljer@gmail.com>
2016-09-12 12:21:15 +0300

    String problem - bug fixed.

--- src/strings_2039/WordCounter.java
+++ src/strings_2039/WordCounter.java
@@ -1,14 +1,23 @@
 package strings_2039;
 
-import java.util.Scanner;
-/**
- * Created by Yrwing on 11.09.2016.
- */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.regex.*;
+
 public class WordCounter {
-    public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        String[] Words = s.nextLine().split("([^a-zA-Z]+)");
-        int n = Words.length;
-        System.out.println(n);
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String str = in.readLine();
+        String[] Words = str.split("([^a-zA-Z]+)");
+        Pattern p = Pattern.compile("([^a-zA-Z]+)");
+        Matcher m = p.matcher(str);
+        if(m.lookingAt()) {
+            System.out.println(Words.length - 1);
+        }
+        else{
+            System.out.println(Words.length);
+        }
     }
 }
