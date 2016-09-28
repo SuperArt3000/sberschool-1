7cd2d1bf83c9be473f34d993a857c3b933bb4760
Votun <cabaljer@gmail.com>
2016-09-28 17:54:00 +0300

    Lesson5 -  Terminal

--- src/ru/sbt/Lesson5_Eceptions/PinValidator.java
+++ src/ru/sbt/Lesson5_Eceptions/PinValidator.java
@@ -1,5 +1,8 @@
 package ru.sbt.Lesson5_Eceptions;
 
+import javax.security.auth.login.AccountLockedException;
+import java.util.Scanner;
+
 /**
  * Created by ���� on 25.09.2016.
  */
@@ -14,24 +17,38 @@ public class PinValidator {
          try{
              if (key != this.server.AccountMap.get(name).PIN){
              counter++;
-             throw new IncorrectPINException();
+             throw new IncorrectPINException("Incorrect PIN.");
             }
              else return true;
          }
-         catch(IncorrectPINException e){
-             if(counter < 3)
-                System.out.println("Incorrect PIN");
-             else try {
+         catch(IncorrectPINException e) {
+             if (counter < 3)
+                 System.out.println("Incorrect PIN");
+             else {
                  System.out.println("Incorrect PIN. Access blocked for 5 seconds.");
                  counter = 0;
-                 Thread.sleep(5000);
-             } catch (InterruptedException exc) {
-                 exc.printStackTrace();
+                 try {AccessBlock();}
+                 catch(AccountLockedException ALEx){
+                     System.out.println(ALEx.getMessage());
+                 }
              }
              return false;
          }
     }
-    private class IncorrectPINException extends Throwable{}
+    private void AccessBlock() throws AccountLockedException{
+        Scanner s = new Scanner(System.in);
+        try {
+            for (int i = 10; i > 0; i--) {
+                Thread.sleep(500);
+                if (s.hasNext()) throw new AccountLockedException((0.5*i) + " seconds remain.");
+            }
+        } catch (InterruptedException exc) {
+            exc.printStackTrace();
+        }
 
+    }
+    public class IncorrectPINException extends Throwable{
+        public IncorrectPINException(String msg){super(msg);}
+    }
 }
 
