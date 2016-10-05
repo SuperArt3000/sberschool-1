4ef98bd38f8e23d4a440b5e51128dea6245550d2
Votun <cabaljer@gmail.com>
2016-10-05 14:17:28 +0300

    Lesson7 -  Proxy

--- src/ru/sbt/Lesson7_proxy/Test.java
+++ src/ru/sbt/Lesson7_proxy/Test.java
@@ -5,17 +5,30 @@ import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * Created by Yrwing on 04.10.2016.
- */
+
 public class Test {
     public static void main(String[] args) {
         AImpl AImpl_test = new AImpl();
+        A_B_Impl ABtest = new A_B_Impl();
+        B_C_Impl BCtest = new B_C_Impl();
         try{
             CashProxy CProxyImpl = new CashProxy(Test.class);
+
             A A_proxy = (A) CProxyImpl.newCashProxy(AImpl_test);
+            C C_proxy = (C) CProxyImpl.newCashProxy(BCtest);
+            A A_proxy2 = (A) CProxyImpl.newCashProxy(ABtest);
+
             System.out.println(A_proxy.out(3));
             System.out.println(A_proxy.out(5));
+            System.out.println(A_proxy2.out(5));
+
+            System.out.println(C_proxy.cashedsum(5, 6));
+            System.out.println(C_proxy.cashedsum(5, 6));
+
+
+            B B_proxy = (B) CProxyImpl.newCashProxy(ABtest);
+            System.out.println(B_proxy.cashedsum(5, 6));
+
         } catch (Exception e) {
             e.printStackTrace();
         }
