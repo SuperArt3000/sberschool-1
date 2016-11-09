249dbfbdee0e9cd961f8ca1043fd5fe89608f7bc
Votun <cabaljer@gmail.com>
2016-11-09 23:52:43 +0300

    SchoolChat, based on UDP

---
+++ src/main/java/ru/sbt/SchoolChat/Server.java
@@ -0,0 +1,105 @@
+package ru.sbt.SchoolChat;
+
+
+import java.io.IOException;
+import java.net.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+
+public class Server implements Runnable{
+    private final int LGN = 0;
+    private final int MSG = 1;
+    private final int GETALLMSG = 2;
+    private final int EXIT = 3;
+    private final Map<String, SocketAddress> addressBook = new HashMap<>();
+    private volatile boolean online;
+    private InetAddress group;
+    private MulticastSocket socket;
+    private  Map<SocketAddress, List<String>> history;
+
+    public static void main(String[] args) {
+        Server server = new Server();
+        server.run();
+    }
+    public void run(){
+          online = true;
+          history = new ConcurrentHashMap<>();
+        try{
+            socket = new MulticastSocket(1234);
+            group = InetAddress.getByName("239.255.255.0");
+            socket.joinGroup(group);
+            byte[] buf = new byte[256];
+            DatagramPacket datagram = new DatagramPacket(buf, buf.length);
+            do {
+                socket.receive(datagram);
+                processDatagram(datagram);
+
+            }while(online);
+            socket.leaveGroup(group);
+
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+    private void addAdress(String login, SocketAddress address){
+        addressBook.put(login, address);
+    }
+
+    private void processDatagram(DatagramPacket dpMessage) throws IOException {
+        String line = new String(dpMessage.getData(), 0, dpMessage.getLength());
+        switch(Integer.parseInt(line.substring(0, 1))) {
+            case LGN:
+                addAdress(line.substring(1), dpMessage.getSocketAddress());
+                annotate(line + " joined the chat.");
+                System.out.println(line.substring(1) + " joined the chat.");
+                break;
+            case GETALLMSG:
+                //sendHistory(dpMessage.getSocketAddress());
+                break;
+            case MSG:
+                sendToReceiver(dpMessage);
+                System.out.println(line.substring(1));
+                break;
+            case EXIT:
+                annotate(line + " left the chat.");
+                addressBook.remove(line.substring(1));
+                System.out.println(line.substring(1) + " left the chat.");
+                break;
+            default:
+                throw new IOException("Unknown command.");
+        }
+    }
+
+    private void annotate(String message){
+        byte[] data = message.getBytes();
+        addressBook.forEach((name, address) -> {
+            DatagramPacket datagram = new DatagramPacket(data, data.length, address);
+            try {
+                socket.send(datagram);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+
+   /* private void sendHistory(SocketAddress receiverAddress){
+        history.get(receiverAddress).
+    }*/
+
+    private void sendToReceiver(DatagramPacket dpMessage) throws IOException {
+        String line = new String(dpMessage.getData(), 0, dpMessage.getLength());
+        line = line.substring(1);
+        SocketAddress address = addressBook.get(line.split(">>>")[1].split("::")[0]);
+        if(address != null) {
+            dpMessage.setSocketAddress(address);
+        }
+        else{
+            dpMessage.setData("Receiver is offline or does not exist.".getBytes());
+        }
+        socket.send(dpMessage);
+    }
+}
---
+++ src/main/java/ru/sbt/SchoolChat/User.java
@@ -0,0 +1,97 @@
+package ru.sbt.SchoolChat;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class User {
+    private final int LGN = 0;
+    private final int MSG = 1;
+    private final int GETALLMSG = 2;
+    private final int EXIT = 3;
+
+    private String name;
+    private volatile boolean connected;
+    public static void main(String[] args){
+        User user = new User();
+        try {
+            user.run();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private void run() throws IOException {
+         connected = true;
+        InetAddress group = InetAddress.getByName("239.255.255.0");
+        try (DatagramSocket socket = new DatagramSocket();
+             BufferedReader console = new BufferedReader(new InputStreamReader(System.in))) {
+            joinChat(console, socket, group);
+            byte[] buf = new byte[256];
+            DatagramPacket datagram = new DatagramPacket(buf, buf.length);
+            requests(console, socket, group);
+            ReentrantLock lock  = new ReentrantLock();
+            do {
+                try {
+                    socket.receive(datagram);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                String command = new String(datagram.getData(), 0, datagram.getLength());
+                System.out.println(command.substring(1));
+            } while (connected);
+            System.out.println("Good Bye!");
+        }
+
+    }
+
+    private void joinChat(BufferedReader console,DatagramSocket socket, InetAddress group) throws IOException {
+        System.out.print("Login: ");
+        name = console.readLine();
+        String login = LGN + name;
+        byte[] loginBytes = login.getBytes();
+        DatagramPacket authorization = new DatagramPacket(loginBytes, loginBytes.length, group, 1234);
+        socket.send(authorization);
+        connected = true;
+    }
+
+    private void newMessage(BufferedReader console,DatagramSocket socket, InetAddress group) throws IOException{
+        String line = console.readLine();
+        if(!line.equalsIgnoreCase("exit")){
+            line = MSG + name + ">>>" + line;
+            byte[] bytes = line.getBytes();
+            DatagramPacket dp = new DatagramPacket(bytes, bytes.length, group, 1234);
+            socket.send(dp);
+        }else{
+            line = EXIT + name;
+            byte[] bytes = line.getBytes();
+            DatagramPacket dp = new DatagramPacket(bytes, bytes.length, group, 1234);
+            socket.send(dp);
+            connected = false;
+        }
+    }
+
+    private void requests(BufferedReader console,DatagramSocket socket, InetAddress group)throws SocketException{
+        ExecutorService outputThread = Executors.newSingleThreadExecutor();
+
+        Runnable printMessage = () -> {
+            do {
+                try {
+                    newMessage(console, socket, group);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } while (connected);
+        };
+        outputThread.execute(printMessage);
+        outputThread.shutdown();
+    };
+
+}
